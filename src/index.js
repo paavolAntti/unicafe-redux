a43356eeac316d673bac86014699ce0915314ddf
Antti Paavola <antti.paavis@gmail.com>
2020-02-19 18:47:57 +0200

    added tests and rest of the apps functionality

--- src/index.js
+++ src/index.js
@@ -26,16 +26,59 @@ const App = () => {
 		  type: 'ZERO'
 	  })
   }
+  const showAll = (
+	store.getState().good + store.getState().ok + store.getState().bad
+  )
 
+  const showAverage = (
+	(store.getState().good-store.getState().bad) / showAll
+  )
+
+  const showPositivePercent = (
+	  (store.getState().good / showAll) * 100
+  )
+if (showAll <= 0) {
+	return (
+		<div>
+			<button onClick={good}>hyvä</button> 
+      		<button onClick={ok}>neutraali </button> 
+      		<button onClick={bad}>huono</button>
+      		<button onClick={zero}>nollaa tilastot</button>
+		</div>
+	)
+}
   return (
     <div>
       <button onClick={good}>hyvä</button> 
       <button onClick={ok}>neutraali </button> 
       <button onClick={bad}>huono</button>
       <button onClick={zero}>nollaa tilastot</button>
-      <div>hyvä {store.getState().good}</div>
-      <div>neutraali {store.getState().ok}</div>
-      <div>huono {store.getState().bad}</div>
+	  <table>
+		<tr>
+			<th>hyvä</th>
+			<td>{store.getState().good}</td>
+		</tr>
+		<tr>
+			<th>neutraali</th>
+			<td>{store.getState().ok}</td>
+		</tr>
+		<tr>
+			<th>huono</th>
+			<td>{store.getState().bad}</td>
+		</tr>
+		<tr>
+			<th>kaikki</th>
+			<td>{showAll}</td>
+		</tr>
+		<tr>
+			<th>keskiarvo</th>
+			<td>{showAverage}</td>
+		</tr>
+		<tr>
+			<th>myönteiset</th>
+			<td>{showPositivePercent} %</td>
+		</tr>
+	  </table>
     </div>
   )
 }
