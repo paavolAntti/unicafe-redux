038c601677febedb26fd27f226156d01bec1c87c
Antti Paavola <antti.paavis@gmail.com>
2020-02-19 11:08:04 +0200

    initial commit

---
+++ src/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  let newState = {
+	good: state.good,
+	ok: state.ok,
+	bad: state.bad
+}
+  switch (action.type) {
+    case 'GOOD':
+		newState.good +=1
+      return newState
+    case 'OK':
+		newState.ok += 1
+      return newState
+    case 'BAD':
+		newState.bad += 1
+      return newState
+    case 'ZERO':
+		newState = {
+			good: 0,
+			ok: 0,
+			bad: 0
+		}
+      return newState
+    default: return state
+  }
+  
+}
+
+export default counterReducer
